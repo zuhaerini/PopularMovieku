0333207aded3504a10a35d9245498817eec47ac3
DhytoDev <DhytoDev@Fordytas-MacBook-Pro.local>
2017-12-01 16:46:26 +0800

    Implement Butterknife

--- app/src/main/java/com/izadalab/popularmovie/MainActivity.java
+++ app/src/main/java/com/izadalab/popularmovie/MainActivity.java
@@ -2,12 +2,18 @@ package com.izadalab.popularmovie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-
+    @BindView(R.id.rv_movie) RecyclerView rvmovie;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
     }
 }
